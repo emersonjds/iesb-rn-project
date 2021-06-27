4d6d33bc6a62949a56e3e8e5e3d6bcf73f377419
Emerson Silva <emerson@Emersons-MacBook-Pro.local>
2021-06-27 00:34:21 -0300

    config register area

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Text, View, SafeAreaView } from "react-native";
 import { Login } from "./screen/Login";
+import { Register } from "./screen/Register";
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <Login />
+      <Register />
     </SafeAreaView>
   );
 };
---
+++ src/screen/Register/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {View, Text} from 'react-native'
+
+export const Register = () => {
+  return (
+    <View>
+      <Text>
+        Register Area
+      </Text>
+    </View>
+  )
+}
