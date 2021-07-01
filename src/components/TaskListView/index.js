73ea99a3a80b3db88d03f5bb625efa93cf26c727
Emerson Silva <emerson@Emersons-MacBook-Pro.local>
2021-06-30 22:58:22 -0300

    TaskListView Component

---
+++ src/components/TaskListView/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  TouchableOpacity,
+  SectionList
+} from 'react-native';
+
+import {
+  Container,
+  HeaderTagText,
+  HeaderText,
+  HeaderContainer,
+  ItemContainer,
+  HeaderTagContainer,
+  ItemTitle
+} from './styles';
+
+const TaskListView = () => {
+  const _renderSectionHeader = (sectionData) => (
+    <HeaderContainer>
+      <HeaderTagContainer>
+        <HeaderTagText>
+          {
+            sectionData.section.title.substr(0, 1)
+          }
+        </HeaderTagText>
+      </HeaderTagContainer>
+      <HeaderText>
+        {
+          sectionData.section.title
+        }
+      </HeaderText>
+    </HeaderContainer>
+  )
+
+  const _renderItem = (itemData) => (
+    <TouchableOpacity>
+      <ItemContainer>
+        <ItemTitle>
+          {itemData.item.title}
+        </ItemTitle>
+      </ItemContainer>
+    </TouchableOpacity>
+  )
+
+  return (
+    <SectionList
+      renderSectionHeader={(section) => _renderSectionHeader(section)}
+      sections={[
+        {
+          data: tasks.filter(data => {
+            return data.priority
+          }),
+          key: "highPriority",
+          title: 'High Priority'
+        },
+        {
+          data: tasks.filter(data => {
+            return !data.priority
+          }),
+          key: 'lowPriority',
+          title: 'Low Priority'
+        }
+      ]}
+      renderItem={(data) => _renderItem(data)} />
+  );
+}
+
+export default TaskListView;
---
+++ src/components/TaskListView/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  flex-direction: column;
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const HeaderContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  background-color: silver;
+  border-radius: 25px;
+  margin-top: 10px;
+`;
+
+export const HeaderTagContainer = styled.View`
+  background-color: gray;
+  height: 35px;
+  width: 35px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 25px;
+`;
+
+export const HeaderTagText = styled.Text`
+  color: #fff;
+  font-size: 22px;
+`;
+
+export const HeaderText = styled.Text`
+  font-size: 16px;
+  margin-left: 10px;
+`;
+
+export const ItemContainer = styled.View`
+  flex: 1;
+  flex-direction: column;
+  background-color: #f3f2f0;
+  margin-top: 5px;
+  padding: 10px;
+  height: 75px;
+`;
+
+export const ItemTitle = styled.Text`
+  font-size: 22px;
+`;
