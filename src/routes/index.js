5110d2dee9863b1a84c0845c28c70eae96861e89
Emerson Silva <emerson_jdss@hotmail.com>
2021-07-10 16:04:52 -0300

    remove unused screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Login } from '../screen/Login';
 import { Register } from '../screen/Register';
-import ToDoTasks from '../screen/TodoTasks'
-import DoneTasks from '../screen/DoneTasks'
+import ToDoTasks from '../screen/TodoTasks';
+import DoneTasks from '../screen/DoneTasks';
 import Task from '../screen/Task';
 import App from '../screen/App';
 
@@ -12,32 +12,37 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 
 const Stack = createStackNavigator();
 
-const Tab = createMaterialTopTabNavigator()
+const Tab = createMaterialTopTabNavigator();
 
 export const TaskTab = () => {
   return (
     <Tab.Navigator
       tabBarOptions={{
-        activeTintColor: 'tomato', inactiveTintColor: 'gray',
-        iconStyle: { width: 20, height: 20 }
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+        iconStyle: { width: 20, height: 20 },
       }}>
       <Tab.Screen name="To Do" component={ToDoTasks} />
       <Tab.Screen name="Done" component={DoneTasks} />
-    </Tab.Navigator >
-  )
-}
+    </Tab.Navigator>
+  );
+};
 
 const Routes = () => {
   return (
     <Stack.Navigator headerMode="screen">
-      <Stack.Screen name="App" component={App} options={{ headerShown: false }} />
+      <Stack.Screen
+        name="App"
+        component={App}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen
         name="Login"
         component={Login}
         options={{ headerShown: false }}
       />
       <Stack.Screen name="Register" component={Register} />
-       <Stack.Screen name="TaskList" component={TaskTab} />
+      <Stack.Screen name="TaskList" component={TaskTab} />
       <Stack.Screen name="Task" component={Task} />
     </Stack.Navigator>
   );
